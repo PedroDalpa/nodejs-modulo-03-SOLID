cef8ea53202973fad842412c48cc7f9624be3518
PedroDalpa <pedro.dalpa@gmail.com>
2024-07-16 13:26:37 -0300

    Add prisma orm on app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    id: crypto.randomUUID(),
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+  },
+})
